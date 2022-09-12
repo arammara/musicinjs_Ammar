48632167573989d7eae04193e3b0ea900dbad126
arammara <109539665+arammara@users.noreply.github.com>
2022-09-12 22:11:01 +0530

    project Done

---
+++ script.js
@@ -0,0 +1,69 @@
+//Mouse Event
+var numberOfDrumButtom = document.querySelectorAll(".drum").length
+console.log("numberOfDrumButtom:"+numberOfDrumButtom);
+
+for(let i =0; i<numberOfDrumButtom ;i++){
+     document.querySelectorAll(".drum")[i].addEventListener('click',
+     function (){  //anonymous function
+          console.log(this);
+          var buttonInnerHTML=this.innerHTML;
+          console.log(buttonInnerHTML);
+          makeSound(buttonInnerHTML);
+          buttonAnimation(buttonInnerHTML); //argument
+     });
+}
+
+//Keyboard Keypress
+document.addEventListener('keypress',
+     function (event){
+          console.log(event);
+          console.log(event.key);
+          makeSound(event.key);
+          buttonAnimation(event.key);
+     }
+);
+
+function makeSound(key){
+    switch(key){
+       case 's':
+            var musicObject = new Audio('assets/tom-1.mp3'); // tom1 is Object
+            musicObject.play();
+       break;
+       case 'r':
+            var musicObject = new Audio('assets/tom-2.mp3'); // tom1 is Object
+            musicObject.play();
+       break;
+       case 'g':
+            var musicObject = new Audio('assets/tom-3.mp3'); // tom1 is Object
+            musicObject.play();
+       break;
+       case 'm':
+            var musicObject = new Audio('assets/tom-4.mp3'); // tom1 is Object
+            musicObject.play();
+       break;
+       case 'p':
+            var musicObject = new Audio('assets/snare.mp3'); // tom1 is Object
+            musicObject.play();
+       break;
+       case 'd':
+            var musicObject = new Audio('assets/crash.mp3'); // tom1 is Object
+            musicObject.play();
+       break;
+       case 'n':
+            var musicObject= new Audio('assets/kick-bass.mp3'); // tom1 is Object
+            musicObject.play();
+       break;
+
+    }
+}
+
+function buttonAnimation(currentKey){
+     var activeButton = document.querySelector("."+currentKey);
+     activeButton.classList.add("pressed");
+
+     setTimeout(
+          function(){
+               activeButton.classList.remove("pressed");
+          }
+          ,100);
+}
